23fb4eff28f2a272020282fac9f8e1cd8c3ce3f4
Petr <dat.petr@gmail.com>
2021-03-21 12:01:51 +0300

    1

--- weather/apps/weather_app/urls.py
+++ weather/apps/weather_app/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from . import views
-from .db_weather import 
+
 
 app_name = 'weather_app'
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('', views.index, name='index'),
 ]
--- weather/apps/weather_app/views.py
+++ weather/apps/weather_app/views.py
@@ -1,8 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .db_weather import diamond, \
+    forest, garden, northern, \
+    polar, port_city, \
+    resort, scientific, \
+    seaside, south, \
+    stepnoy, taiga, western
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse('test')
+
+
+def diamond_(request):
+    return render(request, 'weather_app/test.html')
+
+
+def forest_(request):
+    return render(request, 'weather_app/forest.html')
+
+
+def garden_(request):
+    return render(request, 'weather_app/garden.html')
+
+
+def northern_(request):
+    return render(request, 'weather_app/northern.html')
+
+
+def polar_(request):
+    return render(request, 'weather_app/polar.html')
+
+
+def resort_(request):
+    return render(request, 'weather_app/resort.html')
+
+
+def scientific_(request):
+    return render(request, 'weather_app/scientific.html')
+
+
+def seaside_(request):
+    return render(request, 'weather_app/seaside.html')
+
+
+def south_(request):
+    return render(request, 'weather_app/south.html')
+
+
+def stepnoy_(request):
+    return render(request, 'weather_app/stepnoy.html')
+
+
+def taiga_(request):
+    return render(request, 'weather_app/taiga.html')
+
+
+def western_(request):
+    return render(request, 'weather_app/western.html')
+
+
+def polar_city_(request):
+    return render(request, 'weather_app/polar_city.html')
